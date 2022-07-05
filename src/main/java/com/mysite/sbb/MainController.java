ff0b8999f2d63fea4f98794bae6d3216f22eba44
noHost01 <ljh12081308@naver.com>
2022-07-05 10:53:00 +0900

    세팅

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @RequestMapping("/sbb")
+    @ResponseBody
+    public String index(){
+        return "하이 헬로우";
+    }
+
+
+}
